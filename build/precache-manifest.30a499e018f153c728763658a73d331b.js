6682f6eb74217755bb3b24b263fbf7435a14359b
litiegen <517206296@qq.com>
2019-11-28 08:34:49 +0800

    a

---
+++ build/precache-manifest.30a499e018f153c728763658a73d331b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c456827d991295ebfd6b6b184bf13681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c9faedeb5d32ab0b4e8",
+    "url": "/static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "a30f3873d3f8df67629a",
+    "url": "/static/css/main.f1e513f2.chunk.css"
+  },
+  {
+    "revision": "1c9faedeb5d32ab0b4e8",
+    "url": "/static/js/2.aa0e472c.chunk.js"
+  },
+  {
+    "revision": "a30f3873d3f8df67629a",
+    "url": "/static/js/main.2237b86f.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  },
+  {
+    "revision": "043d99aaaf88c7854dc43c7cbae40596",
+    "url": "/static/media/collection_content_img.043d99aa.jpg"
+  },
+  {
+    "revision": "84273b5f1e5323494da195cf79a526ce",
+    "url": "/static/media/mine_message_img.84273b5f.jpg"
+  }
+]);
